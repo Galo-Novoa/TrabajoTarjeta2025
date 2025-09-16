bdd7bc41a193bf374a1ee571b345153b8a165f5c
galo-novoa <galocnovoa@gmail.com>
2025-09-16 16:01:30 -0300

    TarjetaApp ya no es ignorado por Git

---
+++ TarjetaApp/Tarjeta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoTarjeta2025
+{
+    internal class Tarjeta
+    {
+        private decimal saldo { get; set; }
+        private decimal[] valoresValidos = { 2000m, 3000m, 4000m, 5000m, 8000m, 10000m, 15000m, 20000m, 25000m, 30000m };
+        public Tarjeta(decimal saldo) {
+            this.saldo = saldo;
+        }
+        public decimal getSaldo() { return saldo; }
+
+        public void cargarSaldo(Tarjeta tarjeta,  decimal monto)
+        {
+            if (valoresValidos.Contains(monto)) {
+                tarjeta.saldo = tarjeta.saldo + monto;
+            }
+        }
+        
+    }
+}
