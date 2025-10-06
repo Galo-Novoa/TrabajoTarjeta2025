ddefc37ca53ce25fa54ae8dddd24c00b750bf27c
Galo Novoa <galocnovoa@gmail.com>
2025-10-06 15:08:34 -0300

    README.md viejo

--- TarjetaTest/obj/Debug/net5.0/TarjetaTest.AssemblyInfo.cs
+++ TarjetaTest/obj/Debug/net5.0/TarjetaTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TarjetaTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c3a0f162d1880841c4e89d32a64a2f142dbd47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be00f73555cf6cd61765026e6d76d77dd7ca754")]
 [assembly: System.Reflection.AssemblyProductAttribute("TarjetaTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TarjetaTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
