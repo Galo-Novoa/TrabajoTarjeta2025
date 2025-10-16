1fcb5dac3bb93c350c09db09034407fbfd1a2c4c
Mati-34Mat <matiasberardo07@gmail.com>
2025-10-16 18:57:22 -0300

    merge arreglado

--- TarjetaApp/Tarjeta.cs
+++ TarjetaApp/Tarjeta.cs
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace TarjetaApp
-{
-    internal class Tarjeta
-    {
-        private decimal saldo { get; set; }
-        private static decimal[] cargasAceptadas =
-            { 2000m, 3000m, 4000m, 5000m, 8000m, 10000m, 15000m, 20000m, 25000m, 30000m };
-
-        private List<Boleto> historialViajes;
-
-        public Tarjeta(decimal saldo)
-        {
-            this.saldo = saldo;
-            this.historialViajes = new List<Boleto>();
-        }
-
-        public decimal getSaldo() { return saldo; }
-
-        public List<Boleto> getHistorialViajes() { return historialViajes; }
-
-        public void cargarSaldo(decimal monto)
-        {
-            if (cargasAceptadas.Contains(monto))
-            {
-                saldo += monto;
-                if (saldo > 40000m)
-                    saldo = 40000m;
-
-                Console.WriteLine($"Se cargaron ${monto}. Saldo actual: ${saldo}.");
-            }
-            else
-            {
-                Console.WriteLine($"Monto no aceptado. Valores válidos: {string.Join(", ", cargasAceptadas)}.");
-            }
-        }
-
-        public bool cobrarPasaje(decimal monto)
-        {
-            if (saldo - monto >= 0m)
-            {
-                saldo -= monto;
-                return true;
-            }
-            else
-            {
-                Console.WriteLine($"Saldo insuficiente (${saldo}). Precio del pasaje: ${monto}");
-                return false;
-            }
-        }
-
-        public void agregarBoleto(Boleto boleto)
-        {
-            if (boleto != null)
-                historialViajes.Add(boleto);
-        }
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -83,6 +20,8 @@ namespace TarjetaApp
             this.saldo = saldoInicial;
             this.historialViajes = new List<Boleto>();
         }
+
+        public decimal getSaldo() { return saldo; }
 
         public decimal getSaldo() => saldo;
         public List<Boleto> getHistorialViajes() => historialViajes;
@@ -130,4 +69,3 @@ namespace TarjetaApp
         }
     }
 }
->>>>>>> saldo-negativo
