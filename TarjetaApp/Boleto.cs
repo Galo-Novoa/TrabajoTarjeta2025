e906605ef13a3a0c015cd2de73d82fd04bddfa51
Galo-Novoa <galocnovoa@gmail.com>
2025-11-01 17:39:52 -0300

    Adición de campos nuevos a Boleto.

--- TarjetaApp/Boleto.cs
+++ TarjetaApp/Boleto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,19 @@ namespace TarjetaApp
 {
     internal class Boleto
     {
-        public string linea;
-        public string franquicia;
-        public Boleto(string linea)
+        public string Linea { get; private set; }
+        public string Franquicia { get; private set; }
+        public DateTime Fecha { get; private set; }
+        public  decimal Monto { get; private set; }
+        public int Id { get; private set; }
+        public Boleto(string linea, Tarjeta tarjeta, decimal monto)
         {
-            this.linea = linea;
+            this.Linea = linea;
+            this.Franquicia = tarjeta.Franquicia;
+            this.Monto = monto;
+            this.Id = tarjeta.Id;
+            //Falta implementación de sistema de tiempo para Fecha.
         }
+
     }
 }
