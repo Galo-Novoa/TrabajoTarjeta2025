e32a20e03e4925eca640dcee499b5edcff32bb54
Mati-34Mat <matiasberardo07@gmail.com>
2025-11-02 21:53:58 -0300

    agrega system collections generic

--- TarjetaApp/Franquicias.cs
+++ TarjetaApp/Franquicias.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace TarjetaApp
 {
     internal class BoletoEducativo : Tarjeta
@@ -55,7 +58,6 @@ namespace TarjetaApp
                 return base.CobrarPasaje();
             }
         }
-
     }
 
     internal class MedioBoleto : Tarjeta
@@ -63,5 +65,59 @@ namespace TarjetaApp
         protected override string Franquicia => "Medio Boleto Estudiantil";
         protected override decimal Descuento => 0.5m;
         public MedioBoleto(decimal saldoInicial) : base(saldoInicial) { }
+
+        private Dictionary<DateOnly, int> viajesPorDia = new();
+        private DateTime ultimoViaje = DateTime.MinValue;
+
+        public override bool CobrarPasaje()
+        {
+            DateOnly hoy = DateOnly.FromDateTime(DateTime.Now);
+
+            if ((DateTime.Now - ultimoViaje).TotalMinutes < 5)
+            {
+                Console.WriteLine("Debe esperar al menos 5 minutos entre viajes con medio boleto.");
+                return false;
+            }
+
+            if (!viajesPorDia.ContainsKey(hoy))
+                viajesPorDia[hoy] = 0;
+
+            if (viajesPorDia[hoy] < 2)
+            {
+                if (base.CobrarPasaje())
+                {
+                    viajesPorDia[hoy]++;
+                    ultimoViaje = DateTime.Now;
+                    Console.WriteLine($"Medio boleto aplicado. Viajes con descuento hoy: {viajesPorDia[hoy]}/2");
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Límite de viajes con medio boleto alcanzado. Se cobrará tarifa completa.");
+                decimal descuentoOriginal = this.Descuento;
+
+                var resultado = this.saldo - Colectivo.PrecioPasajeBase >= SaldoMinimo;
+                if (resultado)
+                {
+                    this.saldo -= Colectivo.PrecioPasajeBase;
+                    if (this.saldo < 0)
+                        Console.WriteLine($"Saldo en negativo: ${saldo} (viaje plus utilizado)");
+
+                    if (saldoPendiente > 0m)
+                        AcreditarCarga();
+
+                    ultimoViaje = DateTime.Now;
+                    viajesPorDia[hoy]++;
+                }
+                else
+                {
+                    Console.WriteLine($"No se puede realizar el viaje. Límite de Saldo negativo alcanzado (${saldo}).");
+                }
+
+                return resultado;
+            }
+        }
     }
 }
