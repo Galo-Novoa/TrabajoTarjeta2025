65dee2938aaba6bfd639d3bfbba5ce8adfe3dd13
Galo-Novoa <galocnovoa@gmail.com>
2025-11-04 04:15:41 -0300

    Franquicias renombradas.

--- TarjetaApp/Franquicias.cs
+++ TarjetaApp/Franquicias.cs
@@ -1,35 +1,42 @@
 using System;
+using TarjetaApp;
 
 namespace TarjetaApp
 {
     internal class BoletoEducativo : Tarjeta
     {
-        protected override string Franquicia => "Boleto Educativo Gratuito";
-        protected override decimal Descuento => 0m; // Cambiado a 0m para viajes gratis
-        protected override int ViajesGratisPorDia => 2;
+        protected override string Franquicia => "Completa";
+        protected override decimal Descuento => 1m;
+        protected virtual int ViajesGratisPorDia => 2;
 
         public BoletoEducativo(decimal saldoInicial) : base(saldoInicial) { }
-        public BoletoEducativo(decimal saldoInicial, Tiempo tiempo) : base(saldoInicial, tiempo) { }
     }
 
-    internal class FranquiciaCompleta : Tarjeta
+    internal class Jubilados : Tarjeta
     {
-        protected override string Franquicia => "Franquicia Completa";
-        protected override decimal Descuento => 0m; // Cambiado a 0m para viajes gratis
-        protected override int ViajesGratisPorDia => 2;
+        protected override string Franquicia => "Completa";
+        protected override decimal Descuento => 1m;
+        protected virtual int ViajesGratisPorDia => 2;
 
-        public FranquiciaCompleta(decimal saldoInicial) : base(saldoInicial) { }
-        public FranquiciaCompleta(decimal saldoInicial, Tiempo tiempo) : base(saldoInicial, tiempo) { }
+        public Jubilados(decimal saldoInicial) : base(saldoInicial) { }
     }
 
-    internal class MedioBoleto : Tarjeta
+    internal class MedioEstudiantil : Tarjeta
     {
-        protected override string Franquicia => "Medio Boleto Estudiantil";
+        protected override string Franquicia => "Parcial";
         protected override decimal Descuento => 0.5m;
-        protected override int ViajesGratisPorDia => 0;
-        protected override int MinutosEntreViajes => 5;
-
-        public MedioBoleto(decimal saldoInicial) : base(saldoInicial) { }
-        public MedioBoleto(decimal saldoInicial, Tiempo tiempo) : base(saldoInicial, tiempo) { }
+        protected virtual int MinutosEntreViajes => 5;
+        public MedioEstudiantil(decimal saldoInicial) : base(saldoInicial) { }
     }
+
+
+    internal class MedioUniversitario : Tarjeta
+    {
+        protected override string Franquicia => "Parcial";
+        protected override decimal Descuento => 0.5m;
+        protected virtual int MinutosEntreViajes => 5;
+
+        public MedioUniversitario(decimal saldoInicial) : base(saldoInicial) { }
+    }
+
 }
